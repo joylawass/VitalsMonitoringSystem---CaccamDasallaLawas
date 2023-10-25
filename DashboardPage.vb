e531a3a9c2b653d6313e911708a1ace3b1bd3ebf
criz41241 <dasallacriz@gmail.com>
2023-10-25 14:21:58 +0800

    changed live notifications messages

--- DashboardPage.vb
+++ DashboardPage.vb
@@ -25,14 +25,14 @@
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        NotifyIcon1.ShowBalloonTip(1000, "Device Status", "The device was removed from the patient.", ToolTipIcon.None)
+        NotifyIcon1.ShowBalloonTip(1000, "Device Status", "[Patient] at ward [ward] attempted to remove the device, possible attempted breach.", ToolTipIcon.Warning)
     End Sub
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
-        NotifyIcon1.ShowBalloonTip(1000, "RFID Status", "The patient escaped from the area.", ToolTipIcon.None)
+        NotifyIcon1.ShowBalloonTip(1000, "RFID Status", "[Patient] went through RFID Sensor, possible attempted breach.", ToolTipIcon.Warning)
     End Sub
 
     Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
-        NotifyIcon1.ShowBalloonTip(1000, "Low Battery", "The battery is getting low. 20% remaining.", ToolTipIcon.None)
+        NotifyIcon1.ShowBalloonTip(1000, "Low Battery", "The device assigned to [patient] at ward [ward] is running low on power; needs charging.", ToolTipIcon.Warning)
     End Sub
 End Class
